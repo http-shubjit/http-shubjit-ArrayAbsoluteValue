5f8093f8d9cc9dfa074131b45af99d1e9a54b620
Shubhajit <shubjitgithub@gmail.com>
2024-07-19 07:19:53 +0530

    Done

--- src/main/java/ArrayAbsoluteValue.java
+++ src/main/java/ArrayAbsoluteValue.java
@@ -8,7 +8,17 @@ public class ArrayAbsoluteValue {
      * @param nums an array.
      * @return the absolute value array of nums.
      */
-    public int[] getArrayAbs(int[] nums){
-        return null;
+    public int[] getArrayAbs(int[] nums) {
+int absolute[]=new int[nums.length];
+
+for(int i=0;i<nums.length;i++)
+{
+    if (nums[i] < 0) {
+        absolute[i] = (-nums[i]);
+    } else {
+        absolute[i] = nums[i];
+    }
+}
+return absolute;
     }
 }
